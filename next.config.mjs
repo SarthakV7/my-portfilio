3382018409af00ea70da554dcb89af57169f030c
SarthakV7 <itssarthakvajpayee@gmail.com>
2024-09-25 19:35:11 -0500

    compatibility for gh-pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    output: 'export',
+    basePath: '/my-portfilio',
+    images: {
+      unoptimized: true,
+    },
+  };
+  
+  export default nextConfig;
